075788936c4f9443148e725c6dacb0405a070335
Dominik Teubert <dominik.teubert@bosch.com>
2025-09-02 21:09:31 +0200

    fixed last error

--- lint.py
+++ lint.py
@@ -2,8 +2,8 @@ import re
 import json
 
 def test():
-  l = ''
-  return re.search(r'', l)
+  location = ''
+  return re.search(r'', location)
 
 if __name__ == "__main__":
   print(json.dumps({'Hello':'World'}))
